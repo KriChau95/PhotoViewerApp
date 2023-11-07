35e7631c67cde3bfa9435381c219c00f87b3fa12
Krishaan Chaudhary <krishaan24@gmail.com>
2023-11-07 18:18:26 -0500

    test

---
+++ src/application/Photos.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Photos extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        primaryStage.setTitle("Photos");
+        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.show();
+        primaryStage.setResizable(false);
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/controller/LoginController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField usernameField;
+
+    public void initialize() {
+        // You can add any initialization code here if needed.
+    }
+
+    // You can add event handlers or methods to handle the login process.
+}
