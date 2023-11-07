ea89ffcdf35c7b5752196c29c9bb7ce2dbeeb3ec
Preston Clawson <misteryeh@quarryrun.com>
2023-11-07 00:54:07 -0500

    Basic text input & quit application

--- src/main/java/com/example/photos/HelloController.java
+++ src/main/java/com/example/photos/HelloController.java
@@ -1,14 +1,28 @@
 package com.example.photos;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.Arrays;
 
 public class HelloController {
+    @FXML
+    TextField userInput = new TextField();
+    String username = "";
+
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onQuitButtonClick() {
+        System.exit(0);
+    }
+
+    @FXML
+    protected void onUserEntry(ActionEvent event) {
+        username = userInput.getText();
+        welcomeText.setText("Welcome user " + username + ".");
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.photos {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.photos to javafx.fxml;
     exports com.example.photos;
 }
